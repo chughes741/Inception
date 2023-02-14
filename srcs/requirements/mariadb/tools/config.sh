bd4a3e4e9ed1066198ee6718fdc2d6acd1e3ba58
Cole Hughes <coltenhughes@gmail.com>
2023-02-14 15:37:04 -0500

    Doing a clean retart of wordpress and mariadb

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
+# mkdir -p /run/mysqld
+# chown -R mysql:mysql /run/mysqld
+# chown -R mysql:mysql /var/lib/mysql
 
-mysql_install_db \
-	--user=$MYSQL_USER \
-	--datadir=$MARIA_DB_DIR \
-	$>/dev/null
+# This installs a new table onto the volume
+# mysql_install_db \
+	# --user=$MYSQL_USER \
+	# --datadir=/var/lib/mysql \
+	# $>/dev/null
 
-exec /usr/bin/mysqld --user=$MYSQL_USER
+# init SQL
+# /usr/bin/mysqld --user=mysql --bootstrap
+
+# /usr/bin/mysql mariadb-data < /tmp/load_data.sql
+
+# exec mariadb --user=$MYSQL_USER
+# exec /usr/bin/mysqld --user=$MYSQL_USER
+
+tail -f /dev/null
