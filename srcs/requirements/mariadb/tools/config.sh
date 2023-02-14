bef46407a91dd7da5fe2ca8b9803a066f0ea2bb0
Cole Hughes <coltenhughes@gmail.com>
2023-02-14 13:47:13 -0500

    Got MariaDB starting

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-tail -f /dev/null
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+mysql_install_db \
+	--user=$MYSQL_USER \
+	--datadir=$MYSQL_DB_DIR \
+	$>/dev/null
+
+exec /usr/bin/mysqld --user=$MYSQL_USER
