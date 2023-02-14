50ba4f96a535cdc38a675ddf88a5f32d3462d899
Cole Hughes <coltenhughes@gmail.com>
2023-02-14 13:56:40 -0500

    Added a few more .envs

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -5,7 +5,7 @@ chown -R mysql:mysql /run/mysqld
 
 mysql_install_db \
 	--user=$MYSQL_USER \
-	--datadir=$MYSQL_DB_DIR \
+	--datadir=$MARIA_DB_DIR \
 	$>/dev/null
 
 exec /usr/bin/mysqld --user=$MYSQL_USER
