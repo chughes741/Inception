26c16c170884c999920264e42d6224c1065db27f
Cole Hughes - 42 <coltenhughes@gmail.com>
2023-02-15 15:25:06 -0500

    Added MariaDBd config

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -3,10 +3,10 @@
 # Add info.php page
 echo "<?php echo phpinfo();?>" > /var/www/html/wordpress/info.php
 
-# mariadb \
-	# -h$MYSQL_HOST \
-	# -u$MYSQL_USER \
-	# -p$MYSQL_PASSWORD \
-	# $MYSQL_DATABASE
+mariadb \
+	-h$MYSQL_HOST \
+	-u$MYSQL_USER@localhost \
+	-p$MYSQL_PASSWORD \
+	$MYSQL_DATABASE
 
 tail -f /dev/null
