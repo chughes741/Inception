c074d8c906445fa57451a1e87458c5fe0b1cdf13
Cole Hughes <coltenhughes@gmail.com>
2023-02-14 20:11:42 -0500

    Added sql connection start to wp config

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Add info.php page
 echo "<?php echo phpinfo();?>" > /var/www/html/wordpress/info.php
+
+mariadb \
+	-h$MYSQL_HOST \
+	-u$MYSQL_USER \
+	-p$MYSQL_PASSWORD \
+	$MYSQL_DATABASE
 
 tail -f /dev/null
