c12b5676ac2de8d1bc9b0641488978556ed298cf
Cole Hughes <coltenhughes@gmail.com>
2023-02-20 11:53:48 -0500

    Fixed MariaDB config

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -8,7 +8,7 @@ if [ ! -d "/run/mysqld" ]; then
 	chown -R mysql:mysql /run/mysqld
 fi
 
-# This installs a new table onto the volume on first run
+# Install mariaDB, create table, and setup privileges 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	chown -R mysql:mysql /var/lib/mysql
 
@@ -17,8 +17,21 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 		--user=$MYSQL_USER \
 		--datadir=/var/lib/mysql
 	
-	mariadbd --user=$MYSQL_USER < /tmp/config.sql
+	/usr/bin/mysqld --bootstrap << EOF
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM	mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE $MYSQL_DATABASE;
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
 fi
 
-# Returns PID 1 to Docker then handed to RUN mysqld
+
 exec "$@"
