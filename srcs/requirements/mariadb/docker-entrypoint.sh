1e4982a7366dbd6e27246f87372e4a979ad63b0f
Cole Hughes <coltenhughes@gmail.com>
2023-02-20 13:22:27 -0500

    Cleaned up MariaDB script to do more in image

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 
 set -e
-
-# Install mariaDB, create table, and setup privileges 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	chown -R mysql:mysql /var/lib/mysql
-
-	mysql_install_db \
-		--basedir=/usr \
-		--user=$MYSQL_USER \
-		--datadir=/var/lib/mysql
 	
-	/usr/bin/mysqld --bootstrap << EOF
+/usr/bin/mysqld --bootstrap << EOF
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM	mysql.user WHERE User='';
-DROP DATABASE test;
+DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-CREATE DATABASE $MYSQL_DATABASE;
-CREATE USER '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-fi
+# fi
 
 exec "$@"
